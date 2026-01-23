f74f6b9c2248d65b175786546ecf6aa1afd08d4f
Scott McMaster <scott.d.mcmaster@gmail.com>
2026-01-23 10:48:04 +0800

    feat(darwin): add nix-darwin setup instructions and example flake configuration

---
+++ apps/native/templates/nix-darwin-determinate/flake.nix
@@ -0,0 +1,64 @@
+{
+  # Result of `nix flake init -t nix-darwin/master` command as documented in nix-darwin setup
+  # with some small modifications to make it more turnkey and work with Determinate.
+  description = "Example nix-darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/master";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs =
+    inputs@{
+      self,
+      nix-darwin,
+      nixpkgs,
+    }:
+    let
+      # Get hostname from the current system.
+      hostname = builtins.getEnv "HOSTNAME";
+      hostnameOrDefault = if hostname == null || hostname == "" then "macbook" else hostname;
+
+      configuration =
+        { pkgs, ... }:
+        {
+          networking.hostName = hostnameOrDefault;
+
+          # List packages installed in system profile. To search by name, run:
+          # $ nix-env -qaP | grep wget
+          environment.systemPackages = [
+            pkgs.vim
+          ];
+
+          # Necessary for using flakes on this system.
+          nix.settings.experimental-features = "nix-command flakes";
+
+          # Enable alternative shell support in nix-darwin.
+          # programs.fish.enable = true;
+
+          # Set Git commit hash for darwin-version.
+          system.configurationRevision = self.rev or self.dirtyRev or null;
+
+          # Used for backwards compatibility, please read the changelog before changing.
+          # $ darwin-rebuild changelog
+          system.stateVersion = 6;
+
+          # The platform the configuration will be used on.
+          nixpkgs.hostPlatform = "aarch64-darwin";
+
+          # Required for Determinate:
+          nix.enable = false; # Disable nix-darwin’s Nix management
+        };
+    in
+    {
+      # Build darwin flake using:
+          # Build darwin flake using:
+          # $ darwin-rebuild build --flake .#$(hostname)
+      darwinConfigurations = {
+        (hostnameOrDefault) = nix-darwin.lib.darwinSystem {
+          modules = [ configuration ];
+        };
+      };
+    };
+}
