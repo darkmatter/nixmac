ab39538e11acfcdbd2c567b99c9a92968ff66b5d
Scott McMaster <scott.d.mcmaster@gmail.com>
2026-06-11 10:11:57 +0800

    fix: add serde rename_all attribute to HomebrewCaskItem struct

--- apps/native/src-tauri/src/shared_types/managed_edits.rs
+++ apps/native/src-tauri/src/shared_types/managed_edits.rs
@@ -2,6 +2,7 @@ use serde::{Deserialize, Serialize};
 use specta::Type;
 
 #[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize, Type)]
+#[serde(rename_all = "camelCase")]
 pub struct HomebrewCaskItem {
     pub name: String,
     pub version: Option<String>,
